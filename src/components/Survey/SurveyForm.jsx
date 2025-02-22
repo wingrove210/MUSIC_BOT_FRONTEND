e9ccbec21aca3013c1b7ede4f018904dda6c198f
Merdan-Mahmudow <softprank@gmail.com>
2025-02-22 20:44:21 +0300

    modified:   src/components/Survey/SurveyForm.jsx

--- src/components/Survey/SurveyForm.jsx
+++ src/components/Survey/SurveyForm.jsx
@@ -154,7 +154,7 @@ Email: ${userData.email}
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
-          chat_id: 1372814991,
+          chat_id: 6035406614,
           text: message,
           parse_mode: "Markdown",
           reply_markup: {
